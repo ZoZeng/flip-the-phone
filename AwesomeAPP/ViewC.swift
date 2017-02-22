b620d25a08345076875abf1081ceedbf27eb48fa
ZoZeng <by.zer02nd@gmail.com>
2017-02-21 23:34:37 -0500

    first commit

---
+++ AwesomeAPP/ViewC.swift
@@ -0,0 +1,13 @@
+//
+//  ViewC.swift
+//  AwesomeAPP
+//
+//  Created by zengshihang on 11/2/16.
+//  Copyright © 2016 zengshihang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewC: UIViewController {
+
+}
---
+++ Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  AwesomeAPP
+//
+//  Created by zengshihang on 11/7/16.
+//  Copyright © 2016 zengshihang. All rights reserved.
+//
+
+import Foundation
